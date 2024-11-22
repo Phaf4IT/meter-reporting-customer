3acfaa3b4e6f2d459c7efd3165d34a71c51f8c56
rafaelaphaf <rafaelabreed@gmail.com>
2024-11-22 16:14:44 +0100

    Fixing email???

--- nextjs/lib/authSendRequest.ts
+++ nextjs/lib/authSendRequest.ts
@@ -1,10 +1,12 @@
 export async function sendVerificationRequest(email: string, url: string) {
     console.log("Sending verification request...");
+    console.log(email);
+    console.log(url);
     // Call the cloud Email provider API for sending emails
     const response = await fetch("https://api.mailchannels.net/tx/v1/send", {
         // The body format will vary depending on provider, please see their documentation
         body: JSON.stringify({
-            personalizations: [{to: [{email}]}],
+            personalizations: [{to: [{email: email}]}],
             from: {email: "noreply@company.com"},
             subject: "Sign in to Your page",
             content: [
