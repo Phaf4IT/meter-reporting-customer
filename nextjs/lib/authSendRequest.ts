3e440dc7eb68198244bf68cf52be263e0902af5f
rafaelaphaf <rafaelabreed@gmail.com>
2024-11-22 16:22:33 +0100

    Correct configuration

--- nextjs/lib/authSendRequest.ts
+++ nextjs/lib/authSendRequest.ts
@@ -1,18 +1,19 @@
-export async function sendVerificationRequest(email: string, url: string) {
+export async function sendVerificationRequest(config: Config) {
     console.log("Sending verification request...");
-    console.log(email);
-    console.log(url);
+    console.log(config);
+    console.log(config.identifier);
+    console.log(config.url);
     // Call the cloud Email provider API for sending emails
     const response = await fetch("https://api.mailchannels.net/tx/v1/send", {
         // The body format will vary depending on provider, please see their documentation
         body: JSON.stringify({
-            personalizations: [{to: [{email: email}]}],
+            personalizations: [{to: [{email: config.identifier}]}],
             from: {email: "noreply@company.com"},
             subject: "Sign in to Your page",
             content: [
                 {
                     type: "text/plain",
-                    value: `Please click here to authenticate - ${url}`,
+                    value: `Please click here to authenticate - ${config.url}`,
                 },
             ],
         }),
@@ -24,4 +25,9 @@ export async function sendVerificationRequest(email: string, url: string) {
         const {errors} = await response.json()
         throw new Error(JSON.stringify(errors))
     }
+}
+
+interface Config {
+    identifier: string;
+    url: string
 }
