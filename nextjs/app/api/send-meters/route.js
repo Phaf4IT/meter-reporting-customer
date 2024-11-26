846e55b44a77eca4adde579912c9385f760191af
Phaf4IT <phaf4it@gmail.com>
2024-11-26 20:35:40 +0100

    Update route.js

--- nextjs/app/api/send-meters/route.js
+++ nextjs/app/api/send-meters/route.js
@@ -13,7 +13,18 @@ import {auth} from "@/auth"
 export async function POST(req, res) {
     await auth(req, res)
     if (req.auth) {
-        return Response.status(200).json({})
+        return Response.json({
+            message: "Meters sent successfully"
+        },
+        {
+            status: 200
+        }
+        )
     }
-    return Response.status(400).json({error: "Invalid Credentials"})
+    return Response.json({
+        error: "Invalid Credentials"
+    },
+    {
+        status: 401
+    })
 }
