0fc7c1fe66d9ebe91b993524454200faadc66061
rafaelaphaf <rafaelabreed@gmail.com>
2025-02-10 09:29:17 +0100

    feat: add smtp provider as option

--- nextjs/auth.ts
+++ nextjs/auth.ts
@@ -3,13 +3,37 @@ import getAdapter from "@/components/authjs/auth-adapter";
 import {getAuthorization} from "@/components/authjs/authorization";
 import {getEmailProvider} from "@/components/authjs/email-provider";
 
+const providers = [];
+
+if (process.env.NEXT_RUNTIME !== "edge") {
+    if (process.env.MAIL_PROVIDER === 'smtp') {
+        const {default: Nodemailer} = await import(
+            "next-auth/providers/nodemailer"
+            );
+        providers.push(
+            Nodemailer({
+                server: {
+                    host: process.env.MAIL_HOST,
+                    port: process.env.MAIL_PORT,
+                    auth: {
+                        user: process.env.MAIL_USERNAME,
+                        pass: process.env.MAIL_PASSWORD,
+                    },
+                },
+                from: process.env.EMAIL_FROM,
+            })
+        );
+    } else {
+        providers.push(getEmailProvider());
+    }
+}
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
     adapter: getAdapter(),
     callbacks: {
         authorized: async ({auth, request}) => getAuthorization(auth, request),
     },
-    providers: [getEmailProvider()],
+    providers,
     session:
         {
             maxAge: 4 * 60 * 60 // 4 hours
