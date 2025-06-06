36b95a993c7b26958976240ddef14c7523cc67cf
Rafaela Phaf <rphaf@compassion.nl>
2025-06-06 21:28:40 +0200

    debugging

--- nextjs/testlib/testcontainers/playwright.ts
+++ nextjs/testlib/testcontainers/playwright.ts
@@ -18,7 +18,7 @@ export class PlaywrightContainer {
                 .withWorkingDir("/home/pwuser")
                 .withUser("pwuser")
                 .withPlatform("linux/arm64")
-                .withExposedPorts(3000)
+                // .withExposedPorts(3000)
                 .withCommand([
                     "/bin/sh",
                     "-c",
